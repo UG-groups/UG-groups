354c1b354b76b278b0428258c3b68a46a9febb26
oliverloops <oliveralr@protonmail.com>
2023-04-24 20:58:45 -0600

    working on searchBar layout

--- components/molecules/SearchBar.tsx
+++ components/molecules/SearchBar.tsx
@@ -1,12 +1,18 @@
 import { View, TextInput, StyleSheet, Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 //* Styling
 import Colors from '../../constants/Colors';
 
 export default function SearchBar(){
+    const router = useRouter();
+
     return(
         <View>
             <TextInput style={styles.input} placeholder="Buscar" />
+            <Pressable onPress={() => console.log('search')}>
+                <Ionicons name="md-search" size={28} color={Colors.dark.text} />
+            </Pressable>
         </View>
     )
 }
