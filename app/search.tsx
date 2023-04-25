8b7175d2430a9651ad87badc904fe65608e9eaf6
oliverloops <oliveralr@protonmail.com>
2023-04-24 22:34:10 -0600

    working on search screen

--- app/search.tsx
+++ app/search.tsx
@@ -1,10 +1,42 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '../components/Themed';
+import { Image, ImageSourcePropType, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, SafeAreaView } from '../components/Themed';
+import { FlashList } from '@shopify/flash-list';
+//* UI comps.
+import SearchBar from '../components/molecules/SearchBar';
+
+const { height } = Dimensions.get('window');
+
+const items = [
+  {id: 1, title: "Cine y Cultura", image: require('../assets/images/model_2.png')},
+  {id: 0, title: "Club de Ajedrez UG", image: require('../assets/images/chess.jpg')},
+  {id: 2, title: "Novedades", image: require('../assets/images/model_1.png')},
+  {id: 3, title: "Programación", image: require('../assets/images/model_3.png')},
+]
 
 export default function Search() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Search</Text>
+    <SafeAreaView style={styles.container}>
+      <SearchBar />
+      <View style={styles.grid}>
+        <FlashList 
+          data={items}
+          renderItem={({ item }) => 
+            <ImageItem key={item.id} title={item.title} image={item.image} />
+          }
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          estimatedItemSize={20}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function ImageItem({ title, image }: { title: string, image: ImageSourcePropType }) {
+  return (
+    <View style={styles.imageContainer}>
+      <Text style={styles.cardText}>{title}</Text>
+      <Image source={image} style={styles.image} />
     </View>
   );
 }
@@ -12,16 +44,27 @@ export default function Search() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  grid: {
+    width: '90%',
+    height: height,
+    marginHorizontal: '5%',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  imageContainer: {
+    marginTop: 5,
   },
+  cardText: {
+    position: 'absolute',
+    zIndex: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    fontFamily: 'Basement-Grotesque',
+  },
+  image: {
+    width: 160,
+    height: 205,
+    resizeMode: 'cover',
+    borderRadius: 18,
+    opacity: 0.9,
+  }
 });
--- components/molecules/SearchBar.tsx
+++ components/molecules/SearchBar.tsx
@@ -8,16 +8,38 @@ export default function SearchBar(){
     const router = useRouter();
 
     return(
-        <View>
-            <TextInput style={styles.input} placeholder="Buscar" />
+        <View style={styles.container}>
             <Pressable onPress={() => console.log('search')}>
-                <Ionicons name="md-search" size={28} color={Colors.dark.text} />
+                <Ionicons name="md-search" size={22} color={Colors.pallete.gray} />
             </Pressable>
+            <TextInput 
+                style={styles.input} 
+                placeholder="Buscar por grupo o usuario..." 
+                placeholderTextColor={Colors.pallete.gray}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    input: {}
+    container: {
+        width: "90%",
+        height: 50,
+        flexDirection: 'row',
+        borderRadius: 15,
+        paddingHorizontal: 10,
+        marginHorizontal: "5%",
+        marginVertical: 10,
+        alignItems: 'center',
+        backgroundColor: Colors.pallete.tertiary,
+    },
+    input: {
+        width: "100%",
+        height: 50,
+        paddingLeft: 10,
+        color: Colors.dark.text,
+        fontFamily: 'Montserrat-Medium',
+        borderTopRightRadius: 15,
+        borderBottomRightRadius: 15,
+    }
 });
